a522f886a9a6364bbb4c26473acf504289a106f5
zerorains <me@zerorains.top>
2022-07-04 12:44:19 +0800

    finish task1

--- README.md
+++ README.md
@@ -7,17 +7,60 @@
 
 帧差法：当视频中存在移动物体时，相邻帧之间在灰度上会有所差别，求取两帧图像的灰度差的绝对值，则静止的物体的像素在这个差值结果中的灰度值为0，而移动物体特别是该物体的轮廓处由于存在灰度变化为非0，这样就能大致计算出移动物体的位置，轮廓以及移动路径。
 
-实现代码：
+实现代码：[frame_difference](https://github.com/zeroRains/Task-for-object-chacking/blob/master/Task_1/frame_difference_method.py#L1)
 
-实现效果：
+实现效果：[result.avi](https://github.com/zeroRains/Task-for-object-chacking/raw/master/Task_1/result.avi)
+
+实现过程：根据参考资料2，学习了帧差法的基础概念，因为他加了很多的图像处理方式，比如高斯平滑，腐蚀，膨胀等，我想试试如果单独只用差分法的结果会怎样，下面将展示部分单独使用差分法，使用高斯滤波，使用腐蚀肿胀，控制轮廓边长输出等等情况。我在实现代码中将帧差法封装成类，设置了四个可以变动的参数。
+
+参数说明：
+1. num: 表示帧差法到底差几帧，默认1
+2. thread: 表示侦差后的图像，大于等于这个阈值的像素被保留，否则舍弃，默认10
+3. fps：输出视频的帧率是多少，默认40
+4. contours_len：轮廓的长度超过这个值才会画上检测框，默认60
+
+实现效果截图：
+1. 单独使用差分法（不加任何图像处理方式）:
+
+    ![](images/fig1.png)
+
+    > 图像中小框很多，所以这就用上了我们类中的第四个参数，用来去除很多小框
+
+2. 轮廓边长控制
+
+    ![](images/fig2.png)
+
+    > 经过控制轮廓边长后，很多小框都消失了
+
+3. 高斯平滑+轮廓边长控制
+
+    ![](images/fig3.png)
+    
+    > 好像差别不大
+
+4. 高斯平滑+轮廓边长控制+膨胀+腐蚀
+
+    ![](images/fig4.png)
+    
+    > 好像好多了
+
+（ps：还有很多调了超参的图就不展示了）
+
+参考资料：
+1. [OpenCV 图像处理之膨胀与腐蚀](https://zhuanlan.zhihu.com/p/110330329)
+2. [高斯混合模型与差帧法提取前景](https://blog.csdn.net/qq_45087786/article/details/121865855)
+
 
 ## Task 2:
 
 
-- [ ] 2. 手动复现论文《Visual Object Tracking using Adaptive Correlation Filters. David S. Bolme, J. Ross Beveridge, Bruce A. Draper, Yui Man Lui. CVPR，2010》
+- [ ] 手动复现论文[《Visual Object Tracking using Adaptive Correlation Filters. David S. Bolme, J. Ross Beveridge, Bruce A. Draper, Yui Man Lui. CVPR，2010》](https://ieeexplore.ieee.org/abstract/document/5539960)
+
+论文学习记录：[「论文阅读」Visual Object Tracking using Adaptive Correlation Filters](https://blog.zerorains.top/2022/07/02/%E3%80%8C%E8%AE%BA%E6%96%87%E9%98%85%E8%AF%BB%E3%80%8DVisual-Object-Tracking-using-Adaptive-Correlation-Filters/)
+
 
 ## Task 3:
 
 
-- [ ] 3. 将相关滤波检测算法扩展应用到相似形状物体检测领域。(诸如细胞核检测、汽车检测等场景)
+- [ ] 将相关滤波检测算法扩展应用到相似形状物体检测领域。(诸如细胞核检测、汽车检测等场景)
 
--- Task_1/frame_difference_method.py
+++ Task_1/frame_difference_method.py
@@ -1,34 +1,78 @@
 import cv2
+import copy
 import numpy as np
-import imageio
 
-video = cv2.VideoCapture("../source/demo.mp4")
 
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-fps = video.get(cv2.CAP_PROP_FPS)
-w, h = int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-out = cv2.VideoWriter('./result.avi', fourcc, 20, (w, h), False)
+class FrameDifference:
+    """
+    path：视频的路径
+    num: 帧差法中具体差多少帧，默认1，即1帧过完之后才使用跟踪算法
+    thread：帧差法的阈值，默认10，即当两帧的灰度计算出来后，大于等于thread的会被保留，低于的会被舍弃
+    fps: 追踪后视频的帧率，默认40帧
+    contours_len：轮廓长度超过这个值才会保留这个轮廓
+    """
 
-print(f"width: {w} height: {h}")
-cnt = 0
-frames = []
-while video.isOpened():
-    ret, frame = video.read()
-    if ret == True:
-        cnt += 1
-        print(cnt)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if cnt == 1:
-            pre = gray
-            continue
-        res = cv2.absdiff(pre, gray)
-        frames.append(res)
-        pre = gray
-        out.write(res)
-    else:
-        break
-out.release()
-video.release()
-cv2.destroyAllWindows()
+    def __init__(self, path, num=1, thread=10, fps=40, contours_len=60, show_in_windows=False):
+        self.path = path
+        self.num = num
+        self.thread = thread
+        self.fps = fps
+        self.contours_len = contours_len
+        self.show_in_windows = show_in_windows
 
-imageio.mimsave('result.gif', frames, 'GIF')
+    def run(self, out_path):
+        """
+        :param out_path: 输出跟踪结果的路径
+        :return:
+        """
+        # 视频输出流
+        video = cv2.VideoCapture(self.path)  # 读取原视频
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))  # 宽高
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')  # 设置输出格式
+        out = cv2.VideoWriter(out_path, fourcc, self.fps, (width, height)) # 输出流
+        cnt = 0  # 记录当前多少帧
+        frames = []
+        while video.isOpened():
+            ret, frame = video.read()
+            frac = copy.deepcopy(frame)
+            if ret:
+                cnt += 1
+                frame = cv2.GaussianBlur(frame, (3, 3), 0)  # 高斯平滑
+                print(f"now frame: {cnt}")
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if len(frames) < self.num:
+                    frames.append(gray)
+                    continue
+                diff = cv2.absdiff(frames[0], gray)  # 比较距离为num的两帧之间的差距
+                frames = frames[1:-1]  # 更新帧
+                frames.append(gray)
+                kernel = np.ones((3, 3), np.uint8)  # 创建一个全是1的3x3的滤波核
+                _, diff = cv2.threshold(diff, self.thread, 255, cv2.THRESH_BINARY)  # 阈值选择
+                dilation = cv2.dilate(diff, kernel)  # 膨胀
+                diff = cv2.erode(dilation, kernel)  # 腐蚀
+                # 获取二值化后的轮廓，第一是轮廓，第二个是轮廓属性
+                # 轮廓就是由一个个点构成的，两点之间连线
+                contours, hierarchy = cv2.findContours(diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                for c in contours:
+                    x, y, w, h = cv2.boundingRect(c)  # 获取这些轮廓的框并绘制
+                    tmp = cv2.arcLength(c, True)
+                    if tmp >= self.contours_len:
+                        cv2.rectangle(frac, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                if self.show_in_windows:
+                    cv2.imshow('result', frac)
+                    cv2.waitKey(30)
+                out.write(frac)
+            else:
+                break
+        print(f"{out_path} 视频跟踪完成！")
+        out.release()
+        video.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    path = '../source/demo.mp4'
+    method = FrameDifference(path, show_in_windows=True)
+    method.run('result.avi')
